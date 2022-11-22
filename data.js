70da5835cbe0635608004f41664efdc96fe9e946
ouattara <ouattarayacou494@gmail.com>
2022-11-22 17:01:48 +0000

    first

---
+++ data.js
@@ -0,0 +1,30 @@
+[
+    {
+        Nom:'Ben Lahmer',
+        Prenom:'Fares',
+        Email: 'fres@gmail.com',
+        âge: 26
+    },
+    {
+        Nom:'Kefi',
+        Prenom:'Self',
+        Email: 'kefi@gmail.com',
+        âge: 15
+    },
+    {
+        Nom:'Fatnassi',
+        Prenom:'Sarra',
+        Email: 'sarra.f@gmail.com',
+        âge: 40
+    },
+    {
+        Nom:'Ben Yahla',
+        Prenom:'Rym',
+        âge: 4
+    },
+    {
+        Nom:'Cherif',
+        Prenom:'Saml',
+        âge: 3
+    }
+]
